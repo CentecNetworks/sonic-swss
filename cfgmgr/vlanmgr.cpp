1a4c251a9f64c7b5fcab1b1288f67a3144ee4fde
Yoush <yoush@cenec.com>
2026-01-19 05:29:09 +0000

    [MODIFY:YOUSH][BUG:1873] add vlan id check
    Signed-off-by: Yoush <yoush@cenec.com>

--- cfgmgr/vlanmgr.cpp
+++ cfgmgr/vlanmgr.cpp
@@ -340,6 +340,15 @@ void VlanMgr::doVlanTask(Consumer &consumer)
             continue;
         }
 
+        /* add by yoush for vlan id check in 2026-01-17 */
+        if (vlan_id < 1 || vlan_id > 4094)
+        {
+            SWSS_LOG_WARN("Vlan id is invalid, vlan = %d", vlan_id);
+            it = consumer.m_toSync.erase(it);
+            continue;
+        }
+        /* end by yoush */
+
         string vlan_alias, port_alias;
         string op = kfvOp(t);
 
